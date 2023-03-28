943658b874d06d352ca0d22c0b178355f0cf36ae
Eduardo Eurico de Almeida Gomes <eduardo.eurico11@dinfor.uniceub>
2023-03-28 10:15:53 -0300

    solução2

---
+++ arrays2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nomes = []string{}
+	nomes = append(nomes, "Eurico")
+	fmt.Println(nomes)
+	nomes = append(nomes, "Geonis")
+	fmt.Println(nomes)
+	nomes = append(nomes, "Vinny")
+	fmt.Println(nomes)
+
+}
